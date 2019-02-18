8e552347f55bfae556a4d1b166d0d5597bff72c0
Nicolas Jeker <n.jeker@gmx.net>
2019-02-18 14:01:49 +0100

    Reformat stm32f4xx_it.c file

--- src/stm32f4xx_it.c
+++ src/stm32f4xx_it.c
@@ -1,14 +1,14 @@
 /**
  *****************************************************************************
- * @addtogroup	Project
+ * @addtogroup  Project
  * @{
  *
- * @file		stm32f4xx_it.h
- * @version		1.0
- * @date		2014-11-21
- * @author		rct1
+ * @file        stm32f4xx_it.h
+ * @version     1.0
+ * @date        2014-11-21
+ * @author      rct1
  *
- * @brief		Project Interrupt Request Handler
+ * @brief       Project Interrupt Request Handler
  *
  *****************************************************************************
  * @copyright
@@ -41,10 +41,10 @@ extern "C" {
 #endif /* __cplusplus */
 
 /*----- Header-Files -------------------------------------------------------*/
-#include <stm32f4xx.h>				/* Processor STM32F407IG				*/
-#include <carme.h>					/* CARME Module							*/
-#include <carme_io1.h>				/* CARME IO1 Module						*/
-#include <can.h>					/* CARME CAN Module						*/
+#include <stm32f4xx.h>              /* Processor STM32F407IG                */
+#include <carme.h>                  /* CARME Module                         */
+#include <carme_io1.h>              /* CARME IO1 Module                     */
+#include <can.h>                    /* CARME CAN Module                     */
 #include "stm32f4xx_it.h"
 
 /*----- Macros -------------------------------------------------------------*/
@@ -59,156 +59,133 @@ extern void Default_Handler(void);
 /*----- Implementation -----------------------------------------------------*/
 /**
  *****************************************************************************
- * @brief		This function handles NMI exception.
+ * @brief       This function handles NMI exception.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void NMI_Handler(void) {
+void NMI_Handler(void)
+{
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles Hard Fault exception.
+ * @brief       This function handles Hard Fault exception.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void HardFault_Handler(void) {
-	/* Go to infinite loop when Hard Fault exception occurs */
-	Default_Handler();
+void HardFault_Handler(void)
+{
+        /* Go to infinite loop when Hard Fault exception occurs */
+        Default_Handler();
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles Memory Manage exception.
+ * @brief       This function handles Memory Manage exception.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void MemManage_Handler(void) {
-	/* Go to infinite loop when Memory Manage exception occurs */
-	while (1) {
-	}
+void MemManage_Handler(void)
+{
+        /* Go to infinite loop when Memory Manage exception occurs */
+        while (1) {
+        }
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles Bus Fault exception.
+ * @brief       This function handles Bus Fault exception.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void BusFault_Handler(void) {
-	/* Go to infinite loop when Bus Fault exception occurs */
-	Default_Handler();
+void BusFault_Handler(void)
+{
+        /* Go to infinite loop when Bus Fault exception occurs */
+        Default_Handler();
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles Usage Fault exception.
+ * @brief       This function handles Usage Fault exception.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void UsageFault_Handler(void) {
-	/* Go to infinite loop when Usage Fault exception occurs */
-	Default_Handler();
+void UsageFault_Handler(void)
+{
+        /* Go to infinite loop when Usage Fault exception occurs */
+        Default_Handler();
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles SVCall exception.
+ * @brief       This function handles Debug Monitor exception.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void SVC_Handler(void) {
+void DebugMon_Handler(void)
+{
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles Debug Monitor exception.
+ * @brief       This function handles the EXTI Lines 9:5.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void DebugMon_Handler(void) {
+void EXTI0_IRQHandler(void)
+{
+    if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON3_PIN)) != RESET) {
+        /* CARME IO1 Button3 Interrupt Handler */
+        EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON3_PIN));
+    }
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles PendSVC exception.
+ * @brief       This function handles the EXTI Lines 9:5.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void PendSV_Handler(void) {
+void EXTI9_5_IRQHandler(void)
+{
+    if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON0_PIN)) != RESET) {
+        /* CARME IO1 Button0 Interrupt Handler */
+        EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON0_PIN));
+    }
+
+    if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_CAN_nCAN_INT_PIN)) != RESET) {
+        CARME_CAN_Interrupt_Handler();
+        EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_CAN_nCAN_INT_PIN));
+    }
 }
 
 /**
  *****************************************************************************
- * @brief		This function handles SysTick Handler.
+ * @brief       This function handles the EXTI Lines 15:10.
  *
- * @return		None
+ * @return      None
  *****************************************************************************
  */
-void SysTick_Handler(void) {
-}
+void EXTI15_10_IRQHandler(void)
+{
 
-/**
- *****************************************************************************
- * @brief		This function handles the EXTI Lines 9:5.
- *
- * @return		None
- *****************************************************************************
- */
-void EXTI0_IRQHandler(void) {
+    if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON1_PIN)) != RESET) {
+        /* CARME IO1 Button1 Interrupt Handler */
+        EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON1_PIN));
+    }
 
-	if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON3_PIN)) != RESET) {
-		/* CARME IO1 Button3 Interrupt Handler */
-		EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON3_PIN));
-	}
-}
-
-/**
- *****************************************************************************
- * @brief		This function handles the EXTI Lines 9:5.
- *
- * @return		None
- *****************************************************************************
- */
-void EXTI9_5_IRQHandler(void) {
-
-	if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON0_PIN)) != RESET) {
-		/* CARME IO1 Button0 Interrupt Handler */
-		EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON0_PIN));
-	}
-
-	if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_CAN_nCAN_INT_PIN)) != RESET) {
-		CARME_CAN_Interrupt_Handler();
-		EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_CAN_nCAN_INT_PIN));
-	}
-}
-
-/**
- *****************************************************************************
- * @brief		This function handles the EXTI Lines 15:10.
- *
- * @return		None
- *****************************************************************************
- */
-void EXTI15_10_IRQHandler(void) {
-
-	if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON1_PIN)) != RESET) {
-		/* CARME IO1 Button1 Interrupt Handler */
-		EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON1_PIN));
-	}
-
-	if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON2_PIN)) != RESET) {
-		/* CARME IO1 Button2 Interrupt Handler */
-		EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON2_PIN));
-	}
+    if (EXTI_GetITStatus(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON2_PIN)) != RESET) {
+        /* CARME IO1 Button2 Interrupt Handler */
+        EXTI_ClearITPendingBit(CARME_GPIO_TO_EXTILINE(CARME_IO1_BUTTON2_PIN));
+    }
 }
 
 #ifdef __cplusplus
