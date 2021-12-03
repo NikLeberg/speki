5b5f0c7c1c258830256224400223e8f3197cdceb
reusa1 <reusa1@bfh.ch>
2021-12-03 14:50:57 +0000

    main: test adrian

--- src/main.c
+++ src/main.c
@@ -1,16 +1,14 @@
-#include <stdint.h>
-#include <stm32f4xx.h>
 #include <carme.h>
 #include <carme_io1.h>
+#include <stdint.h>
+#include <stm32f4xx.h>
 
-int main(void)
-{
-        uint8_t switchezzz;
-        CARME_IO1_Init();
-        for(;;)
-        {
-                CARME_IO1_SWITCH_Get(&switchezzz);
-                CARME_IO1_LED_Set(switchezzz,0xFF);
-        }
-        return 0;
+int main(void) {
+    uint8_t switchezzz;
+    CARME_IO1_Init();
+    for (;;) {
+        CARME_IO1_SWITCH_Get(&switchezzz);
+        CARME_IO1_LED_Set(switchezzz, 0xFF);
+    }
+    return 1;
 }
